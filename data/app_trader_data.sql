42d759b2638a86c3af6a7cfe6bc339afa53433da
rileyhigg <rileyh814@gmail.com>
2023-10-03 11:51:20 -0500

    final push

--- data/app_trader_data.sql
+++ data/app_trader_data.sql
@@ -55,13 +55,14 @@ WHERE (name ILIKE '%halloween%'
    OR name ILIKE '%dead%'
    OR name ILIKE '%creepy%'
    OR name ILIKE '%scary%'
-   OR name ILIKE '%monster%')
+   OR name ILIKE '%monster%'
+   OR name ILIKE '%zombie%')
    AND price::money::numeric = 0
    AND rating >= (SELECT AVG (rating)
 				 FROM play_store_apps)
    AND review_count > (SELECT AVG (review_count)
 					  FROM play_store_apps)
-   ORDER BY rating DESC;
+   ORDER BY rating DESC, review_count DESC;
    
 SELECT *
 FROM app_store_apps 
@@ -72,17 +73,40 @@ WHERE (name ILIKE '%halloween%'
    OR name ILIKE '%dead%'
    OR name ILIKE '%creepy%'
    OR name ILIKE '%scary%'
-   OR name ILIKE '%monster%')
+   OR name ILIKE '%monster%'
+   OR name ILIKE '%zombie%')
    AND price = 0
    AND rating >= (SELECT AVG(rating)
 				 FROM app_store_apps)
    AND review_count::numeric > (SELECT AVG (review_count::numeric)
 					  FROM app_store_apps)
-   ORDER BY rating DESC;
+   ORDER BY rating DESC, review_count DESC;
    
+--a. Develop some general recommendations about the price range,
+---genre, content rating, or any other app characteristics that the company should target. 
    
-   
-   
+SELECT name, a.price, (a.review_count::numeric + p.review_count) AS total_review_count, a.rating, p.rating, a.content_rating, primary_genre, install_count 
+FROM app_store_apps AS a INNER JOIN play_store_apps AS p USING (name)
+WHERE a.review_count::numeric > (SELECT AVG (a.review_count::numeric)
+					    FROM app_store_apps AS a)
+  AND p.review_count::numeric > (SELECT AVG (p.review_count::numeric)
+					    FROM play_store_apps AS p);
+						
+WITH top_apps AS (SELECT *
+				FROM app_store_apps
+				WHERE review_count::numeric > (SELECT AVG (review_count::numeric) +100000
+							   FROM app_store_apps)
+				 AND rating > (SELECT AVG (rating) + 0.5
+				  			   FROM app_store_apps)
+				 AND price = 0)
+				 
+SELECT primary_genre, AVG(rating)
+FROM top_apps
+GROUP BY primary_genre
+ORDER BY avg DESC
+						
+		
+						
 
  
 
